4854b1301933e11aa0162f996f9372310d369e0e
gussak <ghussack@gmail.com>
2024-03-25 19:56:51 -0300

    added renamed xmls (that use the UsingDumped overrides) to repo

--- gitgui.sh
+++ gitgui.sh
@@ -2,4 +2,5 @@
 
 cd "`dirname "$0"`"
 (xterm -e bash -c "sleep 1;(nohup git gui & disown);sleep 1"&disown)
-read -t 60 -n 1 -p PressAKeyToExit
+read -t 3 -n 1 -p PressAKeyToExit
+#read -t 60 -n 1 -p PressAKeyToExit
