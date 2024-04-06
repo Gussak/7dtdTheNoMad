d1bd1ca51682a537f93d992ca358b4c1abd5cc59
gussak <ghussack@gmail.com>
2024-04-05 21:28:58 -0300

    fixed meleeWpnBatonT2StunBaton (as it doesnt accept changing RepairTools)

--- ZZ-010 Ghussak - Tweaks/useDumpedXmls.sh
+++ ZZ-010 Ghussak - Tweaks/useDumpedXmls.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
+# BSD 3-Clause License
+# 
+# Copyright (c) 2023, Gussak(github.com/Gussak,ghussak@www.nexusmods.com)
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#PREPARE_RELEASE:REVIEWED:OK
+
 #set -x
 
-#help TODO: SUGGEST THEM: a good way to not require this script would be for the 7DTD engine store a md5sum of each modlet xml file, and if any of them changes, or some new one shows up, the whole modding (each modlet loaded in order) for that specific xml file basename, would be reprocessed. Otherwise, the dumped xml files would be reused instead of reprocessing all modlets.
+#help TODO: SUGGEST THEM: a good way to not require this script probably would be for the 7DTD engine store a md5sum of each modlet xml file, and if any of them changes, or some new one shows up, the whole modding (each modlet loaded in order) for that specific xml file basename, would be reprocessed. Otherwise, the dumped xml files would be reused instead of reprocessing all modlets.
+
+source ./libSrcCfgGenericToImport.sh #place exported arrays above this include
 
 if [[ "${1-}" == "--help" ]];then egrep "[#]help" "$0";exit 0;fi
 
@@ -45,8 +78,8 @@ function FUNCprepareDumpedCfgToOverrideAll() {
 	strFlReal="${strFlFinal}.SkipOnRelease.xml"
 	
 	if $bUndo;then
-		FUNCexecEcho trash "$strFlFinal"
-		FUNCexecEcho trash "$strFlReal"
+		CFGFUNCexec --noErrorExit trash "$strFlFinal"&&:
+		CFGFUNCexec --noErrorExit trash "$strFlReal"&&:
 		return 0;
 	fi
 	
@@ -63,7 +96,7 @@ function FUNCprepareDumpedCfgToOverrideAll() {
 	FUNCexecEcho chmod -v u+w "${strFlClean}"
 	FUNCexecEcho xmlstarlet ed -L -d '//comment()' "${strFlClean}"
 
-	FUNCexecEcho trash "$strFlFinal"
+	CFGFUNCexec --noErrorExit trash "$strFlFinal"&&:
 	echo >"$strFlFinal" #trunc/create
 
 	echo '
@@ -83,7 +116,7 @@ function FUNCprepareDumpedCfgToOverrideAll() {
 	</GhussakTweaks>
 	' >>"$strFlFinal"
 	
-	FUNCexecEcho trash "$strFlClean"
+	CFGFUNCexec trash "$strFlClean"
 	
 	chmod -v ugo-w "$strFlFinal" # to help on avoiding misediting it
 	ls -l "$strFlFinal"
