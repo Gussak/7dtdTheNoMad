4a7c1db0794675102e5fe6ddec256bcd6a09b6b4
gussak <ghussack@gmail.com>
2024-05-18 22:03:38 -0300

    minimal savegame backuper  only regions with landclaims, bedroll and near the player

--- ZZ-010 Ghussak - Tweaks/7dtdMinimalSaveBkp.sh
+++ ZZ-010 Ghussak - Tweaks/7dtdMinimalSaveBkp.sh
@@ -51,21 +51,31 @@ IFS=$'\n' read -d '' -r -a astrListCoords < <(cat "$strFlPlayersXml" |egrep "lpb
 # -1,-1 0,-1
 
 strRegionRegex=""
+iMarginWarning=0
 for strCoord in "${astrListCoords[@]}";do
 	echo
 	echo "[bkp chunk]"
 	
 	eval "$strCoord" # x z
 	
-	xC=$((x/512));
-	zC=$((z/512));
-	if((x<0));then ((xC-=1))&&:;fi
-	if((z<0));then ((zC-=1))&&:;fi
+	: ${iChunkSize:=512} #help I think this is hardcoded and fixed tho
+	xC=$((x/iChunkSize));
+	zC=$((z/iChunkSize));
+	
+	#help remainder xR zR are absolute values to easy calc
+	xR=$((x%iChunkSize))
+	zR=$((z%iChunkSize))
+	
+	if((x<0));then ((xC-=1))&&:; ((xR*=-1))&&:;fi
+	if((z<0));then ((zC-=1))&&:; ((zR*=-1))&&:;fi
+	
 	strChunk="$xC.$zC"
 	
-	#ISSUE: too near the limit of a chunk region may fail to sucessfully backup where NPCs are left to wait. so if xR or zR is < 12 or > 500, this problem may happen, right?
-	xR=$((x%512))
-	zR=$((z%512))
+	: ${iMargin:=32} #help safety margin to warn in case of the possibility of not making a good enough backup
+	if(( xR < iMargin || zR < iMargin || xR > (iChunkSize-iMargin) || zR > (iChunkSize-iMargin) ));then
+		echo "[[[ WARNING ]]] landclaims and bedroll near limit of chunk region will probably fail to backup stuff that goes to a connected chunk there, so if it is in the middle of a building and the building center is in the crossing of 4 nearby chunks, only 1/4 of the building will be backuped!! Also too near the limit of a chunk region may fail to sucessfully backup where NPCs are left to wait? so if xR or zR is < $iMargin or > $iChunkSize-$iMargin, these problems may happen."
+		((iMarginWarning++))&&:
+	fi
 	
 	declare -p strCoord x z strChunk xR zR
 	
@@ -76,14 +86,38 @@ for strCoord in "${astrListCoords[@]}";do
 done
 declare -p strRegionRegex
 
-IFS=$'\n' read -d '' -r -a astrListFlToBkp < <(find "${strSaveFolder}/" -type f |sort -u |egrep -v "/(DynamicMeshes|ConfigsDump|Region)/"; find "${strSaveFolder}/" -type f |sort -u |egrep    ".*/Region/r[.].*${strRegionRegex}[.]7rg$";)&&:;declare -p astrListFlToBkp |tr '[' '\n'
+IFS=$'\n' read -d '' -r -a astrListFlToBkp < <(
+	(
+		find "${strSaveFolder}/" -type f |sort -u |egrep -v "/(DynamicMeshes|ConfigsDump|Region)/";
+		find "${strSaveFolder}/" -type f |sort -u |egrep    ".*/Region/r[.].*${strRegionRegex}[.]7rg$";
+		echo "./Saves/profiles.sdf";
+		ls "${strSaveFolder}/Region/r."*".7rg" -1t |head -n 4; # the newest files are where the player is TODO: try read this from player files, binary data probably. would be better in case of multiplayer also.
+	) |sort -u
+)&&:
 
-astrListFlToBkp+=("./Saves/profiles.sdf")
+#astrListFlToBkp+=("./Saves/profiles.sdf")
+
+#IFS=$'\n' read -d '' -r -a astrListPlayerNear < <(ls "${strSaveFolder}/Region/r."*".7rg" -1t |head -n 4)&&:;declare -p astrListPlayerNear |tr '[' '\n'
+#astrListFlToBkp+=("${astrListPlayerNear[@]}")
+
+
+
+declare -p astrListFlToBkp |tr '[' '\n'
+
+trash "7dtdSaveMinimalBkp.tar.7z"&&:
 
 tar -cf "7dtdSaveMinimalBkp.tar" "${astrListFlToBkp[@]}"
+tar --list -f "7dtdSaveMinimalBkp.tar"
 7z a "7dtdSaveMinimalBkp.tar.7z" "7dtdSaveMinimalBkp.tar"
+
 trash "7dtdSaveMinimalBkp.tar"
 ls -l "7dtdSaveMinimalBkp.tar.7z"
 7z l "7dtdSaveMinimalBkp.tar.7z"
 
-read -n 1 -t 60 -p "press a key to exit (60s)"
+if((iMarginWarning>0));then
+	echo "[[[ WARNING ]]]: there happened iMarginWarning=$iMarginWarning"
+fi
+
+echo "press a key to exit (60s)";read -t 60 -n 1&&:
+
+exit 0
