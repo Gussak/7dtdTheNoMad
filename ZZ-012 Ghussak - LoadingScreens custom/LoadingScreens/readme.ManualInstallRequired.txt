d07e7132743f8249363bec2a15d76fb43a5f2c25
gussak <ghussack@gmail.com>
2024-08-18 20:06:19 -0300

    a22: loadingscreens custom

---
+++ ZZ-012 Ghussak - LoadingScreens custom/LoadingScreens/readme.ManualInstallRequired.txt
@@ -0,0 +1,3 @@
+place images or symlinks to in-game screenshots on this folder 
+and update the loadingscreen.xml file manually
+or run the createLoadingScreensFromScreenshots.sh script
--- updateGitignoreFile.sh
+++ updateGitignoreFile.sh
@@ -2,12 +2,18 @@
 
 echo "INFO: this file is to go to git, do not try to edit .gitignore directly then."
 
+chmod u+w .gitignore
 echo '
 ZZ-010 Ghussak - Tweaks/prepare7dtdTNMforReleaseV22.sh
 ZZ-010 Ghussak - Tweaks/_NewestSavegamePath.IgnoreOnBackup
+ZZ-012 Ghussak - LoadingScreens custom/LoadingScreens/*.jpg
+ZZ-012 Ghussak - LoadingScreens custom/LoadingScreens/*.tga
 ZZ-015 Ghussak - NPC Raider Ambush POI replacers/Prefabs.SkipOnRelease
 ' >.gitignore
-
 chmod ugo-w .gitignore
 
+cat .gitignore
+
 ls -l .gitignore
+
+echoc -w -t 60
