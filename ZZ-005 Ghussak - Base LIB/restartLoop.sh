36251217084970ba61f252c05558e25d7f076caf
gussak <ghussack@gmail.com>
2024-08-22 20:35:50 -0300

    a22: npc barter

--- ZZ-005 Ghussak - Base LIB/restartLoop.sh
+++ ZZ-005 Ghussak - Base LIB/restartLoop.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
-function FUNCdropcaches() {
+export strRLGameLog="$WINEPREFIX/drive_c/users/$USER/AppData/LocalLow/The Fun Pimps/7 Days To Die/Player.log"
+export strRLRun="C:/Games/7 Days To Die/7DaysToDie.exe"
+
+function FUNCRLDropcaches() {
 	set -x;
-	sudo dd if=/proc/3/stat of=/proc/sys/vm/drop_caches bs=1 count=1; #OBS.: this seems to suffice, no need for this whole script
+	sudo dd if=/proc/3/stat of=/proc/sys/vm/drop_caches bs=1 count=1;
 	set +x 
-}
+};export -f FUNCRLDropcaches
 
-strGameLog="$WINEPREFIX/drive_c/Program Files (x86)/7 Days To Die/_Logs_.AppData.LocalLow.The Fun Pimps/Player.log"
-strRun="C:/Games/7 Days To Die/7DaysToDie.exe"
+function FUNCRLRun() {
+	WINEwrapperRunNowAlways=true wine64 "$strRLRun";
+	echo "Exited, hit Enter (60s)";
+	read #read -t 60
+};export -f FUNCRLRun
 
-FUNCdropcaches
-while true;do
-	nTmBefore="$(date +"%s")"
-	
-	export WINEwrapperRunNowAlways=true
-	(xterm -e wine64 "$strRun" & disown)
-	
-	while ! pgrep -fa "^${strRun}";do echo "waiting the app start nTmBefore=$nTmBefore";date;sleep 1;done
-	
-	nTmLog=$nTmBefore;
-	while((nTmLog<=nTmBefore));do
-		nTmLog="$(stat -c "%Y" "$strGameLog")";
-		echo "waiting it really start now, and create a new log file";
-		sleep 1;
+function FUNCRLRunLoop() {
+	FUNCRLDropcaches
+	while true;do
+		nTmBefore="$(date +"%s")"
+		
+		(xterm -e bash -c "FUNCRLRun" & disown)
+		
+		while ! pgrep -fa "^${strRLRun}";do echo "waiting the app start nTmBefore=$nTmBefore";date;sleep 1;done
+		
+		nTmLog=$nTmBefore;
+		while((nTmLog<=nTmBefore));do
+			nTmLog="$(stat -c "%Y" "$strRLGameLog")";
+			echo "waiting it really start now, and create a new log file";
+			sleep 1;
+		done
+		
+		tail -F "$strRLGameLog" |while read strLine;do
+			if [[ "$strLine" =~ "at MainMenuMono.Update" ]];then
+				echo "init error, Restarting";
+				pkill -fe "$strRLRun"
+				break
+			else
+				echo -n .
+			fi;
+			
+			if ! pgrep -fa "^${strRLRun}" >/dev/null;then
+				echo "stopped running, user exit? hit Enter to run again"
+				read
+			fi
+		done
+		
+		date;
+		FUNCRLDropcaches
+		echo "waiting a bit before restarting to let it work by chance (10s) press Enter to run now"
+		read -t 10
 	done
-	
-	tail -F "$strGameLog" |while read strLine;do
-		if [[ "$strLine" =~ "at MainMenuMono.Update" ]];then
-			echo "init error, Restarting";
-			pkill -fe "$strRun"
-			break
-		else
-			echo -n .
-		fi;
-	done
-	
-	date;
-	FUNCdropcaches
-	echo "waiting a bit before restarting to let it work by chance (10s) press Enter to run now"
-	read -t 10
-done
+};export -f FUNCRLRunLoop
+
+: ${bRLrunNow:=true}
+if $bRLrunNow;then
+	FUNCRLRunLoop
+fi
