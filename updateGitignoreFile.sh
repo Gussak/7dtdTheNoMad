c02800c3a6b4a4700a3dcb06e7215e52c21ef401
gussak <ghussack@gmail.com>
2024-08-24 18:18:42 -0300

    a22: tuff raiders is actually ok

--- updateGitignoreFile.sh
+++ updateGitignoreFile.sh
@@ -4,16 +4,18 @@ echo "INFO: this file is to go to git, do not try to edit .gitignore directly th
 
 chmod u+w .gitignore
 echo '
-ZZ-010 Ghussak - Tweaks/prepare7dtdTNMforReleaseV22.sh
+*Ghussak - Tweaks/prepare7dtdTNMforReleaseV22.sh
+*Ghussak - Tweaks.ArtWorkOnly
 *.IgnoreOnBackup
-ZZ-012 Ghussak - LoadingScreens custom/Config/loadingscreen.xml
-ZZ-012 Ghussak - LoadingScreens custom/LoadingScreens/*.jpg
-ZZ-012 Ghussak - LoadingScreens custom/LoadingScreens/*.tga
-ZZ-015 Ghussak - NPC Raider Ambush POI replacers/Prefabs
-ZZ-015 Ghussak - NPC Raider Ambush POI replacers/Prefabs.SkipOnRelease
+*Ghussak - LoadingScreens custom/Config/loadingscreen.xml
+*Ghussak - LoadingScreens custom/LoadingScreens/*.jpg
+*Ghussak - LoadingScreens custom/LoadingScreens/*.tga
+*Ghussak - NPC Raider Ambush POI replacers/Prefabs
+*Ghussak - NPC Raider Ambush POI replacers/Prefabs.SkipOnRelease
 */_tmp/*
 */_log/*
 *.OLD
+*.old
 *.xcf
 ' >.gitignore
 chmod ugo-w .gitignore
