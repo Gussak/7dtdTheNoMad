9208d8d97f5cfdfd515df9fb7dec82ecbdfdb422
gussak <ghussack@gmail.com>
2024-08-26 19:35:55 -0300

    improve restart loop script

--- ZZ-005 Ghussak - Base LIB/restartLoop.sh
+++ ZZ-005 Ghussak - Base LIB/restartLoop.sh
@@ -18,23 +18,24 @@ function FUNCRLRun() {
 
 function FUNCRLRunLoop() {
 	FUNCRLDropcaches
+	iRestartCount=0
 	while true;do
 		nTmBefore="$(date +"%s")"
 		
 		(xterm -e bash -c "FUNCRLRun" & disown)
 		
-		while ! pgrep -fa "^${strRLRun}";do echo "waiting the app start nTmBefore=$nTmBefore";date;sleep 1;done
+		while ! pgrep -fa "^${strRLRun}";do echo "iRestartCount=$iRestartCount, waiting the app start nTmBefore=$nTmBefore";date;sleep 1;done
 		
 		nTmLog=$nTmBefore;
 		while((nTmLog<=nTmBefore));do
 			nTmLog="$(stat -c "%Y" "$strRLGameLog")";
-			echo "waiting it really start now, and create a new log file";
+			echo "iRestartCount=$iRestartCount, waiting it really start now, and create a new log file";
 			sleep 1;
 		done
 		
 		tail -F "$strRLGameLog" |while read strLine;do
 			if [[ "$strLine" =~ "at MainMenuMono.Update" ]];then
-				echo "init error, Restarting";
+				echo "iRestartCount=$iRestartCount, init error, Restarting";
 				pkill -fe "$strRLRun"
 				break
 			else
@@ -42,7 +43,7 @@ function FUNCRLRunLoop() {
 			fi;
 			
 			if ! pgrep -fa "^${strRLRun}" >/dev/null;then
-				echo "stopped running, user exit? hit Enter to run again"
+				echo "iRestartCount=$iRestartCount, stopped running, user exit? hit Enter to run again"
 				read
 				break
 			fi
@@ -50,8 +51,10 @@ function FUNCRLRunLoop() {
 		
 		date;
 		FUNCRLDropcaches
-		echo "waiting a bit before restarting to let it work by chance (10s) press Enter to run now"
+		echo "iRestartCount=$iRestartCount, waiting a bit before restarting to let it work by chance (10s) press Enter to run now"
 		read -t 10
+
+		((iRestartCount++))&&:
 	done
 };export -f FUNCRLRunLoop
 
