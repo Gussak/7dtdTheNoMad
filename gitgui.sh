a154e6f36b4d34d86c9cae9e91811502a67ecf2b
gussak <ghussack@gmail.com>
2026-04-24 00:57:19 -0300

    improve starter kit HUD bar for miasma cvar improve buffGSKgodModeFull

--- gitgui.sh
+++ gitgui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd "`dirname "$0"`"
-(xterm -e bash -c "sleep 1;(nohup git gui & disown);sleep 1"&disown)
+(xterm -e bash -c "sleep 1;(nohup git gui & disown);sleep 1"&disown) #it is important to be this way to force gitgui open a gui to ask for password (instead of requesting the password to be input on the terminal)
 set -x
 read -t 60 -n 1 -p PressAKeyToExit
