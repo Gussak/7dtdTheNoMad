a01196e07c778e7ba2a20952411a4f7dd4b70bb6
gussak <ghussack@gmail.com>
2026-04-27 01:01:42 -0300

    CVar shaman power on HUD light up armor torch when sheltered

--- ZZ-100 Ghussak - CORE - TheNoMad Code Base Library/windows.configCVarsOnHUD.sh
+++ ZZ-100 Ghussak - CORE - TheNoMad Code Base Library/windows.configCVarsOnHUD.sh
@@ -38,64 +38,68 @@ source "${strBaseLibPath}/libSrcCfgGenericToImport.sh" --LIBgencodeTrashLast
 
 # TODO: multilines mode
 
-# config each CVar
-#astrNameList=(Battery Miasma SpidersSpawnMini FireFuel)
-#astrLabelList=(Battery Miasma "Spiders' Repelent" "Torch Fuel")
-#astrXUItoolTipIDList=(xuiGSKbatteryPerc xuiGSKmiasmaPerc xuiGSKspiderRepelPerc xuiGSKtorchFuel)
-#astrCVarBN=(fGSKBatteryChargePerc fGSKMiasmaDirtyPerc iGSKSpawnSpiderMiniTmoutPerc fGSKFireFuelPercPerc) #requires ...Warn cvar too
-#astrColorTextList=("[green]" "80,0,0,255" "255,255,255,255" "255,0,0,255")
-#astrColorAlertList=("255,128,128,255" "255,128,128,255" "255,128,128,255" "255,80,0,255")
-#astrColorWarnList=("255,255,0,255" "255,255,0,255" "200,200,0,255" "255,128,0,255")
-#astrColorGoodList=("0,0,255,255" "0,255,255,255" "0,128,255,255" "255,180,0,255")
-
-## while the entry ID can be ignored, the index will still work
 declare -A astrNameListA astrLabelListA astrXUItoolTipIDListA astrCVarBNA astrColorTextListA astrColorAlertListA astrColorWarnListA astrColorGoodListA
 ## KEEP_TEMPLATE !!!!!!!!!!!!!!!
-#astrNameList[]=""
-#astrLabelList[]=""
-#astrXUItoolTipIDListA[]=""
-#astrCVarBNA[]="" #requires ...Warn cvar too
-#astrColorTextListA[]=""
-#astrColorAlertListA[]=""
-#astrColorWarnListA[]=""
-#astrColorGoodListA[]=""
-astrOrder=(Battery Miasma SpidersSM FireFuel)
+#strID=""
+#astrNameListA[$strID]=""
+#astrLabelListA[$strID]=""
+#astrXUItoolTipIDListA[$strID]=""
+#astrCVarBNA[$strID]="" #requires ...Warn cvar too
+#astrColorTextListA[$strID]=""
+#astrColorAlertListA[$strID]=""
+#astrColorWarnListA[$strID]=""
+#astrColorGoodListA[$strID]=""
+astrOrder=(Battery Miasma SpidersSM FireFuel Shaman)
 # Battery
-astrNameListA[Battery]="Battery"
-astrLabelListA[Battery]="Battery"
-astrXUItoolTipIDListA[Battery]="xuiGSKbatteryPerc"
-astrCVarBNA[Battery]="fGSKBatteryChargePerc"
-astrColorTextListA[Battery]="[green]"
-astrColorAlertListA[Battery]="255,128,128,255"
-astrColorWarnListA[Battery]="255,255,0,255"
-astrColorGoodListA[Battery]="0,0,255,255"
+strID=Battery
+astrNameListA[$strID]="Battery"
+astrLabelListA[$strID]="Battery"
+astrXUItoolTipIDListA[$strID]="xuiGSKbatteryPerc"
+astrCVarBNA[$strID]="fGSKBatteryChargePerc"
+astrColorTextListA[$strID]="[green]"
+astrColorAlertListA[$strID]="255,128,128,255"
+astrColorWarnListA[$strID]="255,255,0,255"
+astrColorGoodListA[$strID]="0,0,255,255"
 # Miasma
-astrNameListA[Miasma]="Miasma"
-astrLabelListA[Miasma]="Miasma"
-astrXUItoolTipIDListA[Miasma]="xuiGSKmiasmaPerc"
-astrCVarBNA[Miasma]="fGSKMiasmaDirtyPerc"
-astrColorTextListA[Miasma]="80,0,0,255"
-astrColorAlertListA[Miasma]="255,128,128,255"
-astrColorWarnListA[Miasma]="255,255,0,255"
-astrColorGoodListA[Miasma]="0,255,255,255"
+strID=Miasma
+astrNameListA[$strID]="Miasma"
+astrLabelListA[$strID]="Miasma"
+astrXUItoolTipIDListA[$strID]="xuiGSKmiasmaPerc"
+astrCVarBNA[$strID]="fGSKMiasmaDirtyPerc"
+astrColorTextListA[$strID]="80,0,0,255"
+astrColorAlertListA[$strID]="255,128,128,255"
+astrColorWarnListA[$strID]="255,255,0,255"
+astrColorGoodListA[$strID]="0,255,255,255"
 # SpidersSM
-astrNameListA[SpidersSM]="SpidersSM"
-astrLabelListA[SpidersSM]="Spiders' Repelent"
-astrXUItoolTipIDListA[SpidersSM]="xuiGSKspiderRepelPerc"
-astrCVarBNA[SpidersSM]="iGSKSpawnSpiderMiniTmoutPerc"
-astrColorTextListA[SpidersSM]="255,255,255,255"
-astrColorAlertListA[SpidersSM]="255,128,128,255"
-astrColorWarnListA[SpidersSM]="200,200,0,255"
-astrColorGoodListA[SpidersSM]="0,128,255,255"
+strID=SpidersSM
+astrNameListA[$strID]="SpidersSM"
+astrLabelListA[$strID]="Spiders' Repelent"
+astrXUItoolTipIDListA[$strID]="xuiGSKspiderRepelPerc"
+astrCVarBNA[$strID]="iGSKSpawnSpiderMiniTmoutPerc"
+astrColorTextListA[$strID]="255,255,255,255"
+astrColorAlertListA[$strID]="255,128,128,255"
+astrColorWarnListA[$strID]="200,200,0,255"
+astrColorGoodListA[$strID]="0,128,255,255"
 # FireFuel
-astrNameListA[FireFuel]="FireFuel"
-astrLabelListA[FireFuel]="Torch Fuel"
-astrXUItoolTipIDListA[FireFuel]="xuiGSKtorchFuel"
-astrCVarBNA[FireFuel]="fGSKFireFuelPerc"
-astrColorTextListA[FireFuel]="255,255,255,255"
-astrColorAlertListA[FireFuel]="180,0,0,255"
-astrColorWarnListA[FireFuel]="255,80,0,255"
-astrColorGoodListA[FireFuel]="255,180,0,255"
+strID=FireFuel
+astrNameListA[$strID]="FireFuel"
+astrLabelListA[$strID]="Torch Fuel"
+astrXUItoolTipIDListA[$strID]="xuiGSKtorchFuel"
+astrCVarBNA[$strID]="fGSKFireFuelPerc"
+astrColorTextListA[$strID]="255,255,255,255"
+astrColorAlertListA[$strID]="180,0,0,255"
+astrColorWarnListA[$strID]="255,80,0,255"
+astrColorGoodListA[$strID]="255,180,0,255"
+# ShamanPower !!!!!!!!!!!!!!!
+strID=Shaman
+astrNameListA[$strID]="Shaman"
+astrLabelListA[$strID]="Shaman Power"
+astrXUItoolTipIDListA[$strID]="xuiGSKshamanPower"
+astrCVarBNA[$strID]="iGSKNearDeathShamanPowerLvlPerc" #requires ...Warn cvar too
+astrColorTextListA[$strID]="255,128,128,255"
+astrColorAlertListA[$strID]="255,128,0,255"
+astrColorWarnListA[$strID]="255,255,0,255"
+astrColorGoodListA[$strID]="0,128,128,255"
 
 # calc the HUD space
 nTotalWidth=300 # could be 450 but the remaining ammo is at that edge
